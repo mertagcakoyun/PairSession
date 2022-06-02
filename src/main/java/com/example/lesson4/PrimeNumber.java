003a1a7b8c284fea3778f042209e2295370a71e4
Mert Agcakoyun <mert.agcakoyun@trendyol.com>
2022-06-02 10:54:08 +0300

    test added

--- src/main/java/com/example/lesson4/PrimeNumber.java
+++ src/main/java/com/example/lesson4/PrimeNumber.java
@@ -5,23 +5,25 @@ import java.util.List;
 
 public class PrimeNumber {
 
-    public static List<Integer> isPrime(int number){
+    public static List<Integer> isPrime(int number) {
         ArrayList<Integer> result = new ArrayList<>();
 
-        if (number < 2){
+        if (number < 2) {
             return result;
         }
 
         int i = 2;
-        do{
-           int remainder = number % i;
-           number = number / i;
+        do {
+            int remainder = number % i;
 
-           if(remainder != 0){
-               i++;
-           }
+            if (remainder != 0) {
+                i++;
+            } else {
+                result.add(i);
+                number = number / i;
+            }
 
-        }while(number != 1);
+        } while (number != 1);
 
         return result;
     }
--- src/test/java/com/example/lesson4/primeNumberTest.java
+++ src/test/java/com/example/lesson4/primeNumberTest.java
@@ -13,7 +13,18 @@ class primeNumberTest {
     public void it_should_return_empty_list(){
         List<Integer> expected = new ArrayList<>();
 
-        List<Integer> resultList = PrimeNumber.isPrime(3);
+        List<Integer> resultList = PrimeNumber.isPrime(1);
+
+        assertEquals(expected,resultList);
+
+    }
+
+    @Test
+    public void it_should_return_2_and_3_in_list_for_6(){
+        List<Integer> expected = new ArrayList<>();
+        expected.add(2);
+        expected.add(3);
+        List<Integer> resultList = PrimeNumber.isPrime(6);
 
         assertEquals(expected,resultList);
 
